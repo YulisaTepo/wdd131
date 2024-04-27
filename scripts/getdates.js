dd32bd2516de8880b62ad159f9fb4727a95ed23e
Yulisa Alexandra Tepo Cubas <ytepo055@gmail.com>
2024-04-27 17:02:56 -0500

    Week 01 Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,3 @@
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+/* This display the last modification*/
+document.querySelector("#lastModified").textContent = document.lastModified;
