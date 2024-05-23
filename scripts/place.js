6b15849a8775220a00d163b052c97e5337ea4ee2
Yulisa Alexandra Tepo Cubas <ytepo055@gmail.com>
2024-05-23 00:05:01 -0500

    Country Page

---
+++ scripts/place.js
@@ -0,0 +1,5 @@
+function calculateWindChill(temperature, windSpeed) {
+    return 13.12 + 0.6215 * temperature - 11.37 * (windSpeed ** 0.16) + 0.3965 * temperature * (windSpeed ** 0.16);
+}
+let windChill = calculateWindChill(17, 8);
+document.querySelector("#wind-chill").textContent = parseFloat(windChill).toFixed(2);
